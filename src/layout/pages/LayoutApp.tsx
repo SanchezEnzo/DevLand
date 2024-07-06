eb58ff42842d716a42c981bcf3714c3adb41dcd9
Enzo Sanchez <enzojsanchez98@gmail.com>
2024-07-06 01:05:12 -0300

    add search

--- src/layout/pages/LayoutApp.tsx
+++ src/layout/pages/LayoutApp.tsx
@@ -1,9 +1,13 @@
+import { Search } from '../components/search/Search';
 import { Sidebar } from '../components/sidebar/Sidebar';
 
 export function LayoutApp() {
 	return (
-		<div className='bg-[rgb(14,14,14)] min-h-screen'>
+		<div className='bg-[rgb(14,14,14)] min-h-screen flex'>
 			<Sidebar />
+			<section className='w-full flex flex-col items-center'>
+				<Search />
+			</section>
 		</div>
 	);
 }
