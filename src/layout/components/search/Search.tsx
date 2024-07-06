e8d654a71a0519edf662417b6883bb03a8272586
Enzo Sanchez <enzojsanchez98@gmail.com>
2024-07-06 10:33:51 -0300

    autocomplete off

--- src/layout/components/search/Search.tsx
+++ src/layout/components/search/Search.tsx
@@ -2,18 +2,19 @@ import { SearchIcon } from '../Icons';
 
 export function Search() {
 	return (
-		<header className='w-[500px] h-16 outline outline-[#f1f1f1aa] outline-[0.1px] mt-5 flex items-center bg-[rgb(11,11,11)] rounded-md'>
-			<form className='text-[#f1f1f1] flex gap-1 items-center w-full h-full pl-4 pr-5 bungee-regular'>
+		<header className='w-[500px] h-12 outline outline-[#f1f1f155] outline-[0.1px] mt-5 flex items-center bg-[rgb(11,11,11)] rounded-md'>
+			<form className='text-[#f1f1f1] flex gap-3 items-center w-full h-full pl-4  bungee-regular'>
 				<button>
 					<SearchIcon />
 				</button>
-				<label htmlFor='search' className='w-full h-full py-4 '>
+				<label htmlFor='search' className='w-full h-full'>
 					<input
 						type='text'
 						name='query'
 						id='search'
 						placeholder='Buscar...'
-						className='w-full h-full bg-transparent focus:outline-none'
+						className='w-full h-full bg-transparent focus:outline-none '
+						autoComplete='off'
 					/>
 				</label>
 			</form>
