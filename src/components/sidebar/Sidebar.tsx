7b5ecdbe3cea41bfdfe149172e4da19ec70d8f82
Enzo Sanchez <enzojsanchez98@gmail.com>
2024-07-07 21:30:20 -0300

    change arquitecture

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -2,14 +2,14 @@ export function Sidebar() {
 	const SECTIONS = ['Todo', 'Iconos', 'Imágenes', 'Tailwindcss', 'UX/UI'];
 
 	return (
-		<aside className='w-full max-w-[300px] bg-[rgb(11,11,11)] flex flex-col items-center h-screen text-[#f1f1f1] gap-10 pt-5'>
+		<aside className='w-full max-w-[300px] bg-[rgb(11,11,11)] flex flex-col items-center h-screen text-[#f1f1f1] gap-10 pt-5 outline outline-[#f1f1f111] outline-[0.1px]'>
 			<h1 className='rubik-doodle-shadow-regular text-5xl'>Devland</h1>
 			<ul className='flex flex-col px-10 w-full gap-5 bungee-regular '>
 				{SECTIONS.map(sec => {
 					return (
 						<li
 							key={sec}
-							className='w-full outline outline-[#f1f1f1aa] bg-[rgb(14,14,14)] outline-[0.1px] rounded-md p-1 cursor-pointer pl-2'
+							className='w-full outline outline-[#f1f1f155] bg-[rgb(14,14,14)] outline-[0.1px] rounded-md p-1 cursor-pointer pl-2'
 						>
 							<span>{sec}</span>
 						</li>
