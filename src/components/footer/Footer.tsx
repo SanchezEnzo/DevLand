58961cb3f753142dcc081731e0741b7ce1fd4596
Enzo Sanchez <enzojsanchez98@gmail.com>
2024-07-11 23:17:49 -0300

    add footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+	return (
+		<section className='h-20 w-full bg-[rgb(11,11,11)] outline outline-[#f1f1f111] outline-[0.1px]'>
+			<nav></nav>
+		</section>
+	);
+}
--- src/pages/LayoutApp.tsx
+++ src/pages/LayoutApp.tsx
@@ -1,4 +1,6 @@
 import { Content } from '../components/content/Content';
+import { Footer } from '../components/footer/Footer';
+import { Pagination } from '../components/pagination/Pagination';
 import { Search } from '../components/search/Search';
 import { Sidebar } from '../components/sidebar/Sidebar';
 
@@ -6,9 +8,11 @@ export function LayoutApp() {
 	return (
 		<div className='bg-[rgb(14,14,14)] flex gutter '>
 			<Sidebar />
-			<div className='w-full flex flex-col items-center '>
+			<div className='w-full flex flex-col items-center relative'>
 				<Search />
 				<Content />
+				<Pagination />
+				<Footer />
 			</div>
 		</div>
 	);
