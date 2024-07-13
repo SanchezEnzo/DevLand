28e84fc7f7f46c8cb4f6c257a895317548280ecc
Enzo Sanchez <enzojsanchez98@gmail.com>
2024-07-13 00:20:25 -0300

    improve optimization in filters

--- src/hooks/useFilter.ts
+++ src/hooks/useFilter.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 import { useEffect, useState } from 'react';
 import { Item } from '../typescript/Interface';
 import { ICONS_ITEM } from '../constants/iconsItems';
@@ -8,14 +9,27 @@ export function useFilter() {
 	const { query } = useQuery();
 	const { category } = useCategory();
 	const [filteredItems, setFilteredItems] = useState<Item[]>();
+
 	useEffect(() => {
-		if (category === 'ALL' && query === '') return;
-		const filterByCategory = ICONS_ITEM.filter(item => item.tag === category);
-		const filterByQuery = filterByCategory.filter(
-			item =>
-				item.name.toLowerCase().includes(query) ||
-				item.description.toLowerCase().includes(query)
-		);
+		if (category === 'ALL' && query === '') {
+			setFilteredItems(ICONS_ITEM);
+			return;
+		}
+
+		const filterByCategory =
+			category === 'ALL'
+				? ICONS_ITEM
+				: ICONS_ITEM.filter(item => item.tag === category);
+
+		const filterByQuery =
+			query === ''
+				? filterByCategory
+				: filterByCategory.filter(
+						item =>
+							item.name.toLowerCase().includes(query.toLowerCase()) ||
+							item.description.toLowerCase().includes(query.toLowerCase())
+				  );
+
 		setFilteredItems(filterByQuery);
 	}, [query, category]);
 
