58a246ca5e077d1ac8966fa9579c2e2b07696677
Enzo Sanchez <enzojsanchez98@gmail.com>
2024-07-30 00:15:43 -0300

    add translate no

--- src/components/search/SearchMobile.tsx
+++ src/components/search/SearchMobile.tsx
@@ -6,7 +6,10 @@ export function SearchMobile() {
 
 	return (
 		<header className='w-full px-5 min-h-12 z-10  flex flex-col items-center  bg-[rgb(27,27,27)]'>
-			<h1 className='rubik-doodle-shadow-regular text-5xl text-[#f1f1f1] pt-5'>
+			<h1
+				className='rubik-doodle-shadow-regular text-5xl text-[#f1f1f1] pt-5'
+				translate='no'
+			>
 				Devland
 			</h1>
 			<form className='text-[#f1f1f1] w-full  gap-3 items-center h-[46px] pl-4 outline  outline-[#f1f1f155] outline-[0.1px] mt-5 flex  bg-[rgb(11,11,11)] rounded-md '>
