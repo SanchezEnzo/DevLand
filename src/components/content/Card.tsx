aa3b2def47be0f635ca4094c026f7d1a7e61feab
Enzo Sanchez <enzojsanchez98@gmail.com>
2024-10-23 16:45:58 -0300

    quit icon link

--- src/components/content/Card.tsx
+++ src/components/content/Card.tsx
@@ -1,9 +1,8 @@
 import { Item } from '../../typescript/Interface';
-import { LinkIcon } from '../Icons';
 
 export function Card({ icon }: { icon: Item }) {
 	return (
-		<article className='outline outline-[#f1f1f125] outline-[0.1px] text-[#f1f1f1ee] rounded-md w-full px-5 py-5 flex flex-col items-center cursor-pointer justify-center gap-3 bg-[rgb(11,11,11)] max-w-[400px] max-md:max-w-[300px] group relative overflow-y-hidden'>
+		<article className='outline outline-[#f1f1f125] outline-[0.1px] text-[#f1f1f1ee] rounded-md w-full px-5 py-5 flex flex-col items-center cursor-pointer justify-center gap-3 bg-[rgb(11,11,11)] max-w-[400px] max-md:max-w-[300px] group relative overflow-y-hidden hover:scale-[1.02] duration-300'>
 			<img
 				src={icon.img}
 				alt={`Captura de pantalla de la página de ${icon.name}`}
@@ -13,14 +12,13 @@ export function Card({ icon }: { icon: Item }) {
 			/>
 			<div className='w-full flex justify-between items-center'>
 				<div className='flex flex-col min-w-3/4 pl-2'>
-					<span className='font-semibold '>{icon.name}</span>
-					<p className='text-[14px] opacity-60 block h-10 text-ellipsis overflow-y-hidden'>
+					<span className='font-semibold '>
+						{icon.name}
+					</span>
+					<p className='text-[14px] opacity-60 block h-10  overflow-y-hidden '>
 						{icon.description}
 					</p>
 				</div>
-				<figure className='opacity-0 group-hover:opacity-100 duration-300 delay-15000'>
-					<LinkIcon />
-				</figure>
 			</div>
 			<a
 				className='absolute w-full h-full z-10 '
