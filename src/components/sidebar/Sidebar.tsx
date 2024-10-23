3336425c09a0678f59bde5ec3b50419e023b5311
Enzo Sanchez <enzojsanchez98@gmail.com>
2024-10-23 16:46:20 -0300

    change color hover

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -12,12 +12,12 @@ export function Sidebar() {
 			>
 				Devland
 			</h1>
-			<ul className='flex flex-col px-10 w-full gap-4 '>
+			<ul className='flex flex-col px-10 w-full gap-4'>
 				{CATEGORIES.map(cat => {
 					return (
 						<li
 							key={cat}
-							className={`w-full outline outline-[rgba(241,241,241,0.2)] outline-[0.1px] rounded-md p-2 cursor-pointer pl-3 font-semibold  duration-300 ${
+							className={`w-full outline outline-[rgba(241,241,241,0.2)] outline-[0.1px] rounded-md p-2 cursor-pointer pl-3 font-semibold hover:bg-[rgba(241,241,241,0.3)] duration-300 ${
 								category === cat
 									? 'bg-[#f1f1f1dd] text-black '
 									: 'bg-[rgb(17,17,17)]'
