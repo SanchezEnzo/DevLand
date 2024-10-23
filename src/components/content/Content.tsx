010a9ccee5a9c83b6575488a0e881cb85e281dca
Enzo Sanchez <enzojsanchez98@gmail.com>
2024-10-23 16:53:55 -0300

    change layout

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -7,7 +7,7 @@ export function Content() {
 	const { page } = usePage();
 
 	return (
-		<section className='px-20 pb-5 max-lg:pb-10 max-md:pb-5 pt-20 max-sm:pt-10 max-md:px-5 gap-5 grid grid-cols-repeatAutoFill max-sm:grid-cols-repeatAutoFillMobile my-0 mx-auto w-full place-items-center min-h-[90dvh] items-start'>
+		<section className='px-20  max-lg:pb-10 max-md:pb-5 pt-16 max-sm:pt-10 max-md:px-5 gap-5 grid grid-cols-repeatAutoFill max-sm:grid-cols-repeatAutoFillMobile my-0 mx-auto w-full place-items-center min-h-[76dvh] items-start'>
 			{filteredItems?.slice(page * 8 - 8, page * 8).map((icon, index) => {
 				return <Card icon={icon} key={index} />;
 			})}
