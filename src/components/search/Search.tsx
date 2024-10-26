baf772c569cc5cd94f85c07c0113d97d37c2db26
Enzo Sanchez <enzojsanchez98@gmail.com>
2024-10-26 20:37:56 -0300

    fix search bug

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -5,8 +5,8 @@ export function Search() {
 	const { handleChange } = useQuery();
 
 	return (
-		<header className='w-[500px] min-h-12 z-10 outline outline-[#f1f1f125] outline-[0.1px] mt-5 flex items-center bg-[rgb(11,11,11)] rounded-md '>
-			<form className='text-[#f1f1f1] flex gap-3 items-center w-full h-full pl-4  '>
+		<header className='w-[500px] h-12 z-10 outline outline-[#f1f1f125] outline-[0.1px] mt-5 flex items-center bg-[rgb(11,11,11)] rounded-md'>
+			<form className='text-[#f1f1f1] flex gap-3 items-center w-full h-full pl-4 m-0'>
 				<div>
 					<SearchIcon />
 				</div>
@@ -16,7 +16,7 @@ export function Search() {
 						name='query'
 						id='search'
 						placeholder='Search'
-						className='w-full h-[46px] bg-transparent focus:outline-none font-semibold'
+						className='w-full h-full bg-transparent focus:outline-none font-semibold'
 						autoComplete='off'
 						onChange={handleChange}
 					/>
