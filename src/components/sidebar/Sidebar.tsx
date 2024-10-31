2cd6ee0b8922356876eaf24bf221c355598d949e
Enzo Sanchez <enzojsanchez98@gmail.com>
2024-10-31 19:07:37 -0300

    change sidebar categories outline

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -17,10 +17,10 @@ export function Sidebar() {
 					return (
 						<li
 							key={cat}
-							className={`w-full outline outline-[rgba(241,241,241,0.2)]  outline-[0.1px] rounded-md p-2 cursor-pointer pl-3 font-semibold  duration-300 ${
+							className={`w-full border-[rgba(241,241,241,0.2)]  border-b-[0.1px] rounded-bl-md p-2 cursor-pointer pl-3 font-semibold  duration-300 ${
 								category === cat
-									? 'bg-[#f1f1f1cc] text-black hover:bg-[#f1f1f1cc] outline-[#f1f1f1] hover:outline-[#f1f1f1]'
-									: 'bg-[rgb(17,17,17)] hover:bg-[#f1f1f133] hover:outline-[#f1f1f177]'
+									? 'bg-[#f1f1f1cc] text-black hover:bg-[#f1f1f1cc] outline-[#f1f1f1] hover:outline-[#f1f1f1] rounded-md translate-x-1'
+									: '  hover:outline-[#f1f1f177] '
 							}`}
 							onClick={() => updateCategory(cat)}
 						>
