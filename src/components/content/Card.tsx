7433a5884a032f0789e873f76b37a322723be056
Enzo Sanchez <enzojsanchez98@gmail.com>
2024-11-10 12:46:49 -0300

    Change outline hover

--- src/components/content/Card.tsx
+++ src/components/content/Card.tsx
@@ -7,7 +7,7 @@ export function Card({ icon, index }: { icon: Item; index: number }) {
 	const { query } = useQuery();
 
 	return (
-		<article className='outline outline-[#f1f1f125] outline-[0.1px] text-[#f1f1f1ee] rounded-md w-full px-5 py-5 flex flex-col items-center cursor-pointer justify-center gap-3 bg-[rgb(11,11,11)] max-w-[400px] max-md:max-w-[300px] group relative overflow-y-hidden sm:hover:scale-[1.012] duration-300'>
+		<article className='outline outline-[#f1f1f111] outline-[0.1px] text-[#f1f1f1ee] rounded-md w-full px-5 py-5 flex flex-col items-center cursor-pointer justify-center gap-3 bg-[rgb(11,11,11)] max-w-[400px] max-md:max-w-[300px] group relative overflow-y-hidden hover:outline-[#f1f1f130] duration-300'>
 			<img
 				src={icon.img}
 				alt={`Screenshot of ${icon.alt}`}
--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -1,27 +1,30 @@
 import { usePage } from '../../hooks/usePage';
 
 export function Pagination() {
-	const { handleNext, handlePrevious, page } = usePage();
-	const { isLastPage } = usePage();
+	const { handleNext, handlePrevious, page, isLastPage } = usePage();
 	return (
 		<section className='w-full max-lg:px-5 px-40 p-10 max-w-[768px] max-lg:max-w-[500px] h-[150px] pb-10  flex justify-between items-center font-semibold gap-2 max-lg:pb-[120px]'>
 			<button
-				className={`w-[150px] max-lg:w-[120px] h-[80px] outline flex items-center justify-center rounded-md outline-[#f1f1f119] outline-[0.1px] duration-300 bg-[rgb(11,11,11)] max-md:h-12 ${
-					page === 1 ? 'text-zinc-700' : 'text-[#f1f1f1] sm:hover:bg-[#1b1b1b]'
+				className={`w-[150px] max-lg:w-[120px] h-[80px] outline flex items-center justify-center rounded-md outline-[#f1f1f111] outline-[0.1px] duration-300 bg-[rgb(11,11,11)] max-md:h-12 ${
+					page === 1
+						? 'text-zinc-700'
+						: 'text-[#f1f1f1] sm:hover:outline-[#f1f1f130]'
 				}`}
 				disabled={page === 1}
 				onClick={handlePrevious}
 			>
 				Previous
 			</button>
-			
-			<div className='h-12 w-12 outline bg-[#f1f1f1dd] text-black  flex justify-center items-center outline-[#f1f1f119] outline-[0.1px] rounded-md border-black border-[3px]'>
+
+			<div className='h-8 w-7 border-[rgba(241,241,241,0.2)]  border-b-[0.1px]  text-[#f1f1f1] flex justify-center items-center   '>
 				{page}
 			</div>
 
 			<button
-				className={`w-[150px] max-lg:w-[120px] h-[80px] outline flex items-center justify-center rounded-md outline-[#f1f1f119] max-md:h-12 outline-[0.1px] duration-300  bg-[rgb(11,11,11)] sm:hover:bg-[#1b1b1b] ${
-					isLastPage === true ? 'text-zinc-700 ' : 'text-[#f1f1f1] '
+				className={`w-[150px] max-lg:w-[120px] h-[80px] outline flex items-center justify-center rounded-md outline-[#f1f1f119] max-md:h-12 outline-[0.1px] duration-300  bg-[rgb(11,11,11)]  ${
+					isLastPage === true
+						? 'text-zinc-700 '
+						: 'text-[#f1f1f1] sm:hover:outline-[#f1f1f130]'
 				}`}
 				onClick={handleNext}
 				disabled={isLastPage}
